52adcf8e003ce0924e832bcee24033227dd08925
Artem Markelov <artem77timoha77@gmail.com>
2024-07-18 15:33:53 +0600

    Поправил урл картинки маленькой карточки.

--- src/features/Products/ProductCart.tsx
+++ src/features/Products/ProductCart.tsx
@@ -1,36 +1,41 @@
 import React from 'react';
-import './Products.css'
-import {createImageUrl} from "../../utils";
-import {useAppSelector} from "../../app/hooks";
-import {selectProductsState} from "./productSlice";
-import imgStarG from '../../assets/Star-grey.png'
+import './Products.css';
+import {createImageUrl} from '../../utils';
+import {useAppSelector} from '../../app/hooks';
+import {selectProductsState} from './productSlice';
+import imgStarG from '../../assets/Star-grey.png';
 
-const ProductCart = () => {
+const ProductCart: React.FC = () => {
   const product = useAppSelector(selectProductsState);
-  const imageUrl = (url: string) => createImageUrl(url);
-  return (
 
+  const imageUrl = product?.images?.[0]?.url ? createImageUrl(product.images[0].url) : '';
+
+  return (
     <div className="div-2">
-      <div className="image-wrapper"><img className="image-2" src={imageUrl(product?.images[0].url || '')}
-                                          alt={product?.title}/></div>
+      <div className="image-wrapper">
+        {imageUrl ? (
+          <img className="image-2" src={imageUrl} alt={product?.title || 'Изображение'}/>
+        ) : (
+          <div className="no-image">Нет изображения</div>
+        )}
+      </div>
       <div className="frame-22">
-        <div className="text-wrapper-26">Jack JK - F4</div>
-        <div className="text-wrapper-27">{product?.category.name}</div>
+        <div className="text-wrapper-26">{product?.title || 'Название товара'}</div>
+        <div className="text-wrapper-27">{product?.category?.name || 'Неизвестная категория'}</div>
         <div className="frame-23">
-          <img className="img-2" src={imgStarG} alt={'star'}/>
-          <img className="img-2" src={imgStarG} alt={'star'}/>
-          <img className="img-2" src={imgStarG} alt={'star'}/>
-          <img className="img-2" src={imgStarG} alt={'star'}/>
-          <img className="img-2" src={imgStarG} alt={'star'}/>
+          <img className="img-2" src={imgStarG} alt="star"/>
+          <img className="img-2" src={imgStarG} alt="star"/>
+          <img className="img-2" src={imgStarG} alt="star"/>
+          <img className="img-2" src={imgStarG} alt="star"/>
+          <img className="img-2" src={imgStarG} alt="star"/>
           <div className="text-wrapper-28">0 отзывов</div>
         </div>
-        <div className="text-wrapper-29">{product?.sellingPrice}</div>
+        <div className="text-wrapper-29">{product?.sellingPrice || 'Цена не указана'}</div>
       </div>
       <div className="primary-btn-2">
         <div className="text-wrapper-15">Купить сейчас</div>
       </div>
     </div>
-
   );
 };
 
